6a9a0a7897f31bdc8bc0939926d09068d945ffb6
osnaider08c <osnaidercavadia@gmail.com>
2023-11-22 23:23:14 -0500

    first commit

---
+++ login.js
@@ -0,0 +1,18 @@
+function loguear()
+{
+
+var user= document.getElementById("usuario").value;
+
+var pass= document.getElementById("pass").value;
+
+if(user=="juan" && pass=="1234")
+
+{
+
+
+window.location="Bienvenida.html";
+
+}
+
+
+}
